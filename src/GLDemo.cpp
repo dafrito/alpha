4a4531591db17b611b0861a1684eb51b55cd837b
Aaron Faanes <dafrito@gmail.com>
2011-10-01 06:03:04 -0500

    Changed all spaces to tabs

--- src/GLDemo.cpp
+++ src/GLDemo.cpp
@@ -10,50 +10,50 @@
 #endif
 
 GLDemo::GLDemo(QWidget *parent)
-    : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
+	: QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
-    xRot = 0;
-    yRot = 0;
-    zRot = 0;
+	xRot = 0;
+	yRot = 0;
+	zRot = 0;
 }
 
 void GLDemo::initializeGL()
 {
-    qglClearColor(Qt::black);
+	qglClearColor(Qt::black);
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_MULTISAMPLE);
-    static GLfloat lightPosition[4] = { 0.5, 5.0, 7.0, 1.0 };
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_MULTISAMPLE);
+	static GLfloat lightPosition[4] = { 0.5, 5.0, 7.0, 1.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 }
 
 void GLDemo::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    glTranslatef(0.0, 0.0, -10.0);
-    glRotatef(xRot / 16.0, 1.0, 0.0, 0.0);
-    glRotatef(yRot / 16.0, 0.0, 1.0, 0.0);
-    glRotatef(zRot / 16.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -10.0);
+	glRotatef(xRot / 16.0, 1.0, 0.0, 0.0);
+	glRotatef(yRot / 16.0, 0.0, 1.0, 0.0);
+	glRotatef(zRot / 16.0, 0.0, 0.0, 1.0);
 }
 
 void GLDemo::resizeGL(int width, int height)
 {
-    int side = qMin(width, height);
-    glViewport((width - side) / 2, (height - side) / 2, side, side);
+	int side = qMin(width, height);
+	glViewport((width - side) / 2, (height - side) / 2, side, side);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 #ifdef QT_OPENGL_ES_1
-    glOrthof(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0);
+	glOrthof(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0);
 #else
-    glOrtho(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0);
+	glOrtho(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0);
 #endif
-    glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 /**
@@ -61,15 +61,15 @@ void GLDemo::resizeGL(int width, int height)
  */
 void GLDemo::keyPressEvent(QKeyEvent *e)
 {
-    if (e->key() == Qt::Key_Escape)
-        close();
-    else
-        QWidget::keyPressEvent(e);
+	if (e->key() == Qt::Key_Escape)
+		close();
+	else
+		QWidget::keyPressEvent(e);
 }
 
 void GLDemo::mousePressEvent(QMouseEvent *event)
 {
-    lastPos = event->pos();
+	lastPos = event->pos();
 }
 
 /**
@@ -77,50 +77,50 @@ void GLDemo::mousePressEvent(QMouseEvent *event)
  */
 void GLDemo::mouseMoveEvent(QMouseEvent *event)
 {
-    int dx = event->x() - lastPos.x();
-    int dy = event->y() - lastPos.y();
+	int dx = event->x() - lastPos.x();
+	int dy = event->y() - lastPos.y();
 
-    if (event->buttons() & Qt::LeftButton) {
-        setXRotation(xRot + 8 * dy);
-        setYRotation(yRot + 8 * dx);
-    } else if (event->buttons() & Qt::RightButton) {
-        setXRotation(xRot + 8 * dy);
-        setZRotation(zRot + 8 * dx);
-    }
-    lastPos = event->pos();
+	if (event->buttons() & Qt::LeftButton) {
+		setXRotation(xRot + 8 * dy);
+		setYRotation(yRot + 8 * dx);
+	} else if (event->buttons() & Qt::RightButton) {
+		setXRotation(xRot + 8 * dy);
+		setZRotation(zRot + 8 * dx);
+	}
+	lastPos = event->pos();
 }
 
 static void qNormalizeAngle(int &angle)
 {
-    while (angle < 0)
-        angle += 360 * 16;
-    while (angle > 360 * 16)
-        angle -= 360 * 16;
+	while (angle < 0)
+		angle += 360 * 16;
+	while (angle > 360 * 16)
+		angle -= 360 * 16;
 }
 
 void GLDemo::setXRotation(int angle)
 {
-    qNormalizeAngle(angle);
-    if (angle != xRot) {
-        xRot = angle;
-        updateGL();
-    }
+	qNormalizeAngle(angle);
+	if (angle != xRot) {
+		xRot = angle;
+		updateGL();
+	}
 }
 
 void GLDemo::setYRotation(int angle)
 {
-    qNormalizeAngle(angle);
-    if (angle != yRot) {
-        yRot = angle;
-        updateGL();
-    }
+	qNormalizeAngle(angle);
+	if (angle != yRot) {
+		yRot = angle;
+		updateGL();
+	}
 }
 
 void GLDemo::setZRotation(int angle)
 {
-    qNormalizeAngle(angle);
-    if (angle != zRot) {
-        zRot = angle;
-        updateGL();
-    }
+	qNormalizeAngle(angle);
+	if (angle != zRot) {
+		zRot = angle;
+		updateGL();
+	}
 }
--- src/GLDemo.h
+++ src/GLDemo.h
@@ -5,40 +5,40 @@
 
 class GLDemo : public QGLWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    GLDemo(QWidget *parent = 0);
-    ~GLDemo() {}
+	GLDemo(QWidget *parent = 0);
+	~GLDemo() {}
 
-    QSize minimumSizeHint() const
-    {
-        return QSize(50, 50);
-    }
+	QSize minimumSizeHint() const
+	{
+		return QSize(50, 50);
+	}
 
-    QSize sizeHint() const
-    {
-        return QSize(400, 400);
-    }
+	QSize sizeHint() const
+	{
+		return QSize(400, 400);
+	}
 
 protected:
-    void initializeGL();
-    void paintGL();
-    void resizeGL(int width, int height);
+	void initializeGL();
+	void paintGL();
+	void resizeGL(int width, int height);
 
-    void keyPressEvent(QKeyEvent *e);
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
+	void keyPressEvent(QKeyEvent *e);
+	void mousePressEvent(QMouseEvent *event);
+	void mouseMoveEvent(QMouseEvent *event);
 
 private:
-    int xRot;
-    int yRot;
-    int zRot;
-    QPoint lastPos;
+	int xRot;
+	int yRot;
+	int zRot;
+	QPoint lastPos;
 
-    void setXRotation(int angle);
-    void setYRotation(int angle);
-    void setZRotation(int angle);
+	void setXRotation(int angle);
+	void setYRotation(int angle);
+	void setZRotation(int angle);
 };
 
 #endif // GLDEMO_H
