7e47aa38b67a892dcaa937be48e3a15e151f5d7e
Aaron Faanes <dafrito@gmail.com>
2011-10-01 22:42:29 -0500

    Removed a lot of effects that were enabled
    Some demos that are in the works mess with these effects in order to
    demonstrate them, so it's best to remove them. The lighting effects can
    also cause issues.

--- src/GLDemo.cpp
+++ src/GLDemo.cpp
@@ -29,20 +29,12 @@ GLDemo::GLDemo(QWidget *parent)
 void GLDemo::initializeGL()
 {
 	qglClearColor(Qt::black);
-
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
 	glEnable(GL_MULTISAMPLE);
-	static GLfloat lightPosition[4] = { 0.5, 5.0, 7.0, 1.0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 }
 
 void GLDemo::paintGL()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 	glTranslatef(0.0, 0.0, -10.0);
 	glRotatef(xRot / ROTATION_SCALE, 1.0, 0.0, 0.0);
