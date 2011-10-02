4fac99b57b977e9e920a00edb0c77ab5c21b2334
Aaron Faanes <dafrito@gmail.com>
2011-10-02 04:35:45 -0500

    Display most recently added widget

--- src/demos.cpp
+++ src/demos.cpp
@@ -19,7 +19,25 @@ int main(int argc, char *argv[])
 	SpiralGLDemo spiralDemo;
 	demoStack.addTab(&spiralDemo, "Spiral");
 
-	demoStack.show();
+	// Display most recently added tab.
+	demoStack.setCurrentIndex(demoStack.count() -1);
+
+	QDesktopWidget* desktop = QApplication::desktop();
+
+	int desktopArea = desktop->width() * desktop->height();
+	int appArea = appSize.width() * appSize.height();
+	if (((float)appArea / (float)desktopArea) > 0.75f) {
+		// Just maximize it if the desktop isn't significantly
+		// bigger than our app's area.
+		demoStack.showMaximized();
+	} else {
+		// Center the app on the primary monitor.
+		QPoint windowLocation = desktop->screenGeometry(desktop->primaryScreen()).center();
+		windowLocation.setX(windowLocation.x() - appSize.width() / 2);
+		windowLocation.setY(windowLocation.y() - appSize.height() / 2);
+		demoStack.move(windowLocation);
+		demoStack.show();
+	}
 
 	return app.exec();
 }
