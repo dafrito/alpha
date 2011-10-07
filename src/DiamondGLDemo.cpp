35a45fe542f9b278073dc593a5b2074fe3884835
Aaron Faanes <dafrito@gmail.com>
2011-10-07 02:53:04 -0500

    Lots of tweaks to the Diamond demo text
    * textmode is now rich-text. This lets us format a lot more
    * Used a better C++-specific convention to create long strings
    * Rewrote a lot of it for clarity

--- src/DiamondGLDemo.cpp
+++ src/DiamondGLDemo.cpp
@@ -18,6 +18,8 @@ DiamondGLDemo::DiamondGLDemo(QWidget* const parent) :
 
 	mainLayout->addWidget(glWidget, 1);
 
+	descriptionLabel->setTextFormat(Qt::RichText);
+
 	QBoxLayout* const options = new QBoxLayout(QBoxLayout::TopToBottom);
 	mainLayout->addLayout(options);
 
@@ -51,7 +53,7 @@ DiamondGLDemo::DiamondGLDemo(QWidget* const parent) :
 
 	QScrollArea* const descriptionScrollPanel = new QScrollArea;
 	descriptionScrollPanel->setWidget(descriptionLabel);
-	descriptionScrollPanel->setFixedWidth(300);
+	descriptionScrollPanel->setFixedWidth(350);
 	descriptionScrollPanel->setWidgetResizable(true);
 	options->addWidget(descriptionScrollPanel);
 
@@ -90,99 +92,101 @@ void DiamondGLDemo::updateDescription()
 {
 	QString text;
 	if (glWidget->usingBackFaceCulling()) {
-		text += "Polygons that are back-facing are being removed. The winding order of ";
-		text += "vertices is used to determine whether a polygon is front- or back-facing. ";
-		text += "A polygon with vertices displayed in a counter-clockwise fashion is said ";
-		text += "to have counter-clockwise winding. If this same polygon is flipped over, its ";
-		text += "vertices would be displayed in a clockwise fashion.";
-		text += "\n\n";
-		text += "While it's arbitrary whether clockwise or counter-clockwise winding ";
-		text += "represents front-facing polygons, it's common to use counter-clockwise. ";
-		text += "This convention matches the right-hand rule used for vectors.";
-		text += "\n\n";
+		text += "<h4>Back-face Culling</h4>"
+			"Polygons that are back-facing are being removed. The facing of a polygon "
+			"is determined using the <b>winding order</b> of its vertices. "
+			"A polygon with vertices displayed in a counter-clockwise fashion is said "
+			"to have counter-clockwise winding. If this same polygon is flipped over, its "
+			"vertices would be displayed in a clockwise fashion."
+			"<p>"
+			"While it is arbitrary whether clockwise or counter-clockwise winding "
+			"represents front-facing polygons, it's common to use the latter. "
+			"This convention matches the <b>right-hand rule</b> used for vectors."
+			"<p>";
 		if (!glWidget->correctingWindingOrder()) {
-			text += "The winding order of the top of the diamond is incorrectly ";
-			text += "set. Since the winding order is used to determine if a ";
-			text += "polygon is back-facing or not, the top is interpreted as ";
-			text += "front-facing when it's really facing away from the camera, ";
-			text += "and vice versa.";
-			text += "\n\n";
-			text += "There are two artifacts of this: the first is the top of the ";
-			text += "diamond is incorrectly culled when it should be visible, ";
-			text += "resulting in a hole. The second is that the top is rendered ";
-			text += "when it should be behind the rest of the figure.";
-			text += "\n\n";
+			text += "<h4>Incorrect Winding Order</h4>"
+				"The winding order of the top of the diamond is incorrectly "
+				"set. Since the winding order is used to determine if a "
+				"polygon is back-facing or not, the top is interpreted as "
+				"front-facing when it's really facing away from the camera, "
+				"and vice versa."
+				"<p>"
+				"There are two visible artifacts of this mistake:"
+				"<ul>"
+				"<li>The top of the diamond is incorrectly hidden when it should be visible"
+				", resulting in a hole."
+				"<li>The top is rendered when it should be behind the rest of the figure. "
+				"This may be corrected by the depth test."
+				"</ul>"
+				"<p>";
 			if (glWidget->usingDepthTesting()) {
-				text += "The first artifact is not correctable (without correcting ";
-				text += "the wind), but the second is corrected via the depth test. ";
-				text += "The depth test is ensuring that the pixels nearest to the ";
-				text += "camera. Since the incorrect winding does not change the depth ";
-				text += "of individual pixels, the depth test is able to hide this ";
-				text += "artifact.";
-				text += "\n\n";
-			} else {
-				text += "While the first artifact is not correctable (without correcting ";
-				text += "the wind), the second would be fixed by enabling the depth test. ";
-				text += "The depth test would ensure only the geometry nearest to the ";
-				text += "camera is rendered. Since the incorrect winding does not change the ";
-				text += "depth of individual pixels, the depth test would correct this ";
-				text += "artifact.";
-				text += "\n\n";
+				text += "<h4>Depth Testing</h4>"
+					"The depth test is ensuring that only the figures closest "
+					"to the camera are being rendered. Without it, figures would "
+					"be rendered in the order that they were provided, indiscriminately "
+					"occluding other geometry. The depth test is able to correct the second "
+					"artifact since the top of the diamond is behind the rest of the figure "
+					"regardless of its winding order."
+					"<p>";
 			}
 		} else if (glWidget->usingDepthTesting()) {
 			assert (glWidget->correctingWindingOrder());
-			text += "The depth test is ensuring that only the figures closest ";
-			text += "to the camera are being rendered. Without it, figures would ";
-			text += "be rendered in the order that they were provided, indiscriminately ";
-			text += "occluding other geometry. However, in very simple cases like this one, ";
-			text += "the back-face test is sufficient to generate a correct image.";
-			text += "\n\n";
+			text += "<h4>Depth Testing</h4>"
+				"The depth test is ensuring that only the figures closest "
+				"to the camera are being rendered. Without it, figures would "
+				"be rendered in the order that they were provided, indiscriminately "
+				"occluding other geometry. However, in very simple cases like this one, "
+				"the back-face test is sufficient to generate a correct image."
+				"<p>";
 		} else {
 			assert(glWidget->usingBackFaceCulling());
 			assert(glWidget->correctingWindingOrder());
 			assert(!glWidget->usingDepthTesting());
-			text += "The depth test is not being performed. Front-facing geometry ";
-			text += "will indiscriminately occlude previously rendered polygons. ";
-			text += "However, in very simple cases like this one, the back-face test ";
-			text += "is sufficient to generate a correct image.";
-			text += "\n\n";
+			text += "<h4>Depth Testing</h4>"
+				"The depth test is not being performed. Front-facing geometry "
+				"will indiscriminately occlude previously rendered polygons. "
+				"However, in very simple cases like this one, the back-face test "
+				"is sufficient to generate a correct image."
+				"<p>";
 		}
-		text += "Since back-face culling removes back-facing polygons, the 'Draw Backs as ";
-		text += "Wireframes' would have no effect.";
+		text += "Since back-face culling removes back-facing polygons, the <b>Draw Backs as ";
+		text += "Wireframes</b> option would have no effect.";
 	} else if (glWidget->usingDepthTesting()) {
-		text += "The depth test is ensuring that only the figures closest ";
-		text += "to the camera are being rendered. Without it, figures would ";
-		text += "be rendered in the order provided, indiscriminately occluding ";
-		text += "other geometry.";
-		text += "\n\n";
+		text += "<h4>Depth Testing</h4>"
+			"The depth test is ensuring that only the figures closest "
+			"to the camera are being rendered. Without it, figures would "
+			"be rendered in the order provided, indiscriminately occluding "
+			"other geometry."
+			"<p>";
 		assert(!glWidget->usingBackFaceCulling());
-		text += "Since the back-face culling test is not being performed, all polygons ";
-		text += "are being rendered. While the depth test is sufficient to render a ";
-		text += "correct looking image, the process is suboptimal.";
-		text += "\n\n";
-		text += "The depth test implicitly removes back-facing pixels, so the 'Draw Backs ";
-		text += "as Wireframes' option is disabled. Also, since the depth test doesn't ";
-		text += "consider winding, the 'Correct Winding Order' option is also disabled.";
-		text += "option is disabled.";
+		text += "Since the back-face culling test is not being performed, all polygons "
+			"are being rendered. While the depth test is sufficient to render a "
+			"correct looking image, the process is suboptimal."
+			"<p>"
+			"The depth test implicitly removes all back-facing pixels in this scene , so "
+			"the <b>Draw Backs as Wireframes</b> option is disabled. Since the depth test "
+			"doesn't consider winding, the <b>Correct Winding Order</b> option is also disabled."
+			"<p>";
 	} else {
 		assert(!glWidget->usingDepthTesting());
 		assert(!glWidget->usingBackFaceCulling());
-		text += "With no depth tests or back-face culling, only the order of rendering ";
-		text += "matters. Polygons are rendered indiscriminately; they will appear on top ";
-		text += "of previously drawn polygons regardless of their geometric relationship. ";
-		text += "For example, the top of the diamond will always appear 'above' the rest of ";
-		text += "the figure.";
-		text += "\n\n";
-		text += "This configuration would appear correct only if we ensured the scene is ";
-		text += "always drawn back-to-front, commonly known as the 'painter's algorithm.'";
-		text += "\n\n";
+		text += "<h4>Painter's Algorithm</h4>"
+			"With no depth tests or back-face culling, only the order of rendering "
+			"matters. Polygons are rendered indiscriminately; they will appear on top "
+			"of previously drawn polygons regardless of their geometric relationship. "
+			"For example, the top of the diamond will always appear in front of the rest of "
+			"the figure."
+			"<p>"
+			"This configuration would appear correct only if we ensured the scene is "
+			"always drawn back-to-front, commonly known as the <i>painter's algorithm</i>."
+			"<p>";
 		if (glWidget->drawingBacksAsWireframes()) {
-			text += "Back-facing polygons are being drawn as wireframes. This simulates ";
-			text += "what the back-face culling operation would do. Enable that test ";
-			text += "for more details on winding and back-facing polygon removal.";
+			text += "Back-facing polygons are being drawn as wireframes. This simulates "
+				"what the back-face culling operation would do. Enable that test "
+				"for more details on winding and back-facing polygon removal.";
 		} else {
-			text += "Since the winding order is not relevant in this configuration, the ";
-			text += "'Correct Winding Order' option has been disabled.";
+			text += "Since the winding order is not relevant in this configuration, the "
+				"<b>Correct Winding Order</b> option has been disabled.";
 		}
 	}
 	descriptionLabel->setText(text);
