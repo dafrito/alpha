d4a744ef36cd81489d7bd3a4b4acf7248e8901ba
Aaron Faanes <dafrito@gmail.com>
2011-10-09 20:28:22 -0500

    Removed a level of indention

--- src/SpiralLinesGLWidget.h
+++ src/SpiralLinesGLWidget.h
@@ -29,24 +29,22 @@ void SpiralLinesGLWidget::render()
 	static const float PI = 3.14159;
 
 	glBegin(GL_LINES); //how do you make GL_LINES not have spaces?
-		//the java code C++'d
-		/*for (float angle = 0; angle < PI ; angle +=  PI / 5.0f) {
-			float x = 50.0f * (float) sin(angle);
-			float y = 50.0f * (float) cos(angle);
-			glVertex2f(x, y);
+	//the java code C++'d
+	/*for (float angle = 0; angle < PI ; angle +=  PI / 5.0f) {
+		float x = 50.0f * (float) sin(angle);
+		float y = 50.0f * (float) cos(angle);
+		glVertex2f(x, y);
 
-			x = 50.0f * (float) sin(PI + angle);
-			y = 50.0f * (float) cos(PI + angle);
-			glVertex2f(x, y);
-		*/
-		//An actual spiral
-		for (float angle = 0; angle < PI*20 ; angle +=  PI / 200.0f) {
-			float x = angle * (float) sin(angle);
-			float y = angle * (float) cos(angle);
-			glVertex2f(x, y);
-		
-		}
-	
+		x = 50.0f * (float) sin(PI + angle);
+		y = 50.0f * (float) cos(PI + angle);
+		glVertex2f(x, y);
+	*/
+	//An actual spiral
+	for (float angle = 0; angle < PI*20 ; angle +=  PI / 200.0f) {
+		float x = angle * (float) sin(angle);
+		float y = angle * (float) cos(angle);
+		glVertex2f(x, y);
+	}
 	glEnd();
 }
 
