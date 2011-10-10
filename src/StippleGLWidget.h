d665b094b0a411d51a04b035764b6fa942cf0f94
Aaron Faanes <dafrito@gmail.com>
2011-10-09 19:49:48 -0500

    StippleGLWidget: removed a level of indention

--- src/StippleGLWidget.h
+++ src/StippleGLWidget.h
@@ -25,29 +25,26 @@ void StippleGLWidget::initializeGL()
 
 void StippleGLWidget::render()
 {
-		glEnable(GL_LINE_STIPPLE);
-		int height = 160; 
-		int length = 100;
-		int lineSpacing = 10;
+	glEnable(GL_LINE_STIPPLE);
+	int height = 160;
+	int length = 100;
+	int lineSpacing = 10;
 
-		int pattern = 0xffff; //bit pattern determines which parts of the line to draw
-		int factor = 3; //how many times to use each bit in the pattern before going to the next
+	int pattern = 0xffff; //bit pattern determines which parts of the line to draw
+	int factor = 3; //how many times to use each bit in the pattern before going to the next
 
 
-		for (int y = -(height/2); y < (height/2); y += lineSpacing) {
-			glLineStipple(factor, pattern);
-			glBegin(GL_LINES);
-			float percentComplete = (float) (y +(height/2)) / height;
-			glColor3f(.3f + percentComplete / 3.0f, 1.0f - percentComplete, percentComplete);
-			glVertex2f(-(length/2) , y );
-			glVertex2f( (length/2), y  );
-			glEnd();
-			factor += 0;
-			pattern -= 0x1111;
-		}
-
-	
-	
+	for (int y = -(height/2); y < (height/2); y += lineSpacing) {
+		glLineStipple(factor, pattern);
+		glBegin(GL_LINES);
+		float percentComplete = (float) (y +(height/2)) / height;
+		glColor3f(.3f + percentComplete / 3.0f, 1.0f - percentComplete, percentComplete);
+		glVertex2f(-(length/2) , y );
+		glVertex2f( (length/2), y  );
+		glEnd();
+		factor += 0;
+		pattern -= 0x1111;
+	}
 }
 
 #endif // STIPPLEGLWIDGET_H
