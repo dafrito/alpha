154ab40d486347065a24d654e758139692b6dcf8
Aaron Faanes <dafrito@gmail.com>
2012-12-25 22:08:02 -0600

    Viewport: Initial stab

--- src/gl/Viewport.cpp
+++ src/gl/Viewport.cpp
@@ -1,11 +1,33 @@
 #include "Viewport.hpp"
 
+#include <GL/gl.h>
+#include "gl/util.hpp"
+
 namespace nt {
 namespace gl {
 
-void Viewport::render()
+void ProjectionPerspective::updatePerspective(const int width, const int height) const
 {
-    // TODO Insert code here.
+    // XXX These should eventually be pulled into the Viewport
+    static const double FOV = 65;
+    static const double VIEW_DISTANCE = 800;
+
+    glViewport(0, 0, width, height);
+
+    GLint oldMatrixMode;
+    glGetIntegerv(GL_MATRIX_MODE, &oldMatrixMode);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gl::setGLFrustum(
+        FOV,
+        (float)(width / height),
+        1,
+        VIEW_DISTANCE
+    );
+
+    // Reset the old mode
+    glMatrixMode(oldMatrixMode);
 }
 
 } // namespace gl
--- src/gl/Viewport.hpp
+++ src/gl/Viewport.hpp
@@ -1,19 +1,50 @@
 #ifndef ALPHA_VIEWPORT_HEADER
 #define ALPHA_VIEWPORT_HEADER
 
+#include <vector>
+#include "gl/RenderLayer.hpp"
+#include "gl/Camera.hpp"
+
 namespace nt {
 namespace gl {
 
-template <typename Scalar, typename Perspective>
+class ProjectionPerspective
+{
+public:
+    void updatePerspective(const int width, const int height) const;
+};
+
+template <typename Scalar, typename Perspective = ProjectionPerspective>
 class Viewport : public Perspective
 {
-    // Camera property
-    // Perspective policy
-    // Render Layers list of layers
+    typedef std::vector<RenderLayer<Scalar>*> RenderLayerList;
 
+    Camera _camera;
+    RenderLayerList _renderLayers;
+
+    Vector3<Scalar> _cameraPos;
 public:
-    void render();
+
+    void render() const
+    {
+        for (typename RenderLayerList::const_iterator layer = _renderLayers.begin(); layer != _renderLayers.end(); ++layer) {
+            (*layer)->render(_cameraPos);
+        }
+    }
+
+    void addRenderLayer(const RenderLayer<Scalar>* const layer)
+    {
+        _renderLayers.push_back(layer);
+    }
+
+    void removeRenderLayer(const RenderLayer<Scalar>* const layer)
+    {
+        typename RenderLayerList::iterator newEnd =
+            std::remove(_renderLayers.begin(), _renderLayers.end(), layer);
+        _renderLayers.erase(newEnd, _renderLayers.end());
+    }
 };
+
 
 } // namespace gl
 } // namespace nt
